ea8575333d59412af7df8f67896322cda6b10999
seyed reza amerizadeh <39367629+amerizade73@users.noreply.github.com>
2021-11-26 13:44:32 +0330

    Update

--- q2.py
+++ q2.py
@@ -12,8 +12,26 @@ class Q2(MRJob):
                 ]
 
     def mapper(self, _, line):
+        stop_words = frozenset(['ourselves', 'hers', 'between', 'yourself', 
+        'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 
+        'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 
+        'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off',
+        'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 
+        'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his',
+        'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 
+        'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
+        'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any',
+        'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 
+        'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why',
+        'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 
+        'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i',
+        'after', 'few', 'whom', 't', 
+        'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it',
+        'how', 'further', 'was', 'here', 'than'])
+
         line = line.strip().translate(str.maketrans('', '', string.punctuation))
         words = line.lower().split()
+        words = [w for w in words if not w.lower() in stop_words]
         for word in words:
             yield word, 1
 
